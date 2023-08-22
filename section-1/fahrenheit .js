06cd7e34f00c66227fc6a9c141f2ba982054cf99
Omkar Sharma <omkarsharma2821@gmail.com>
2023-08-23 00:15:35 +0530

    practice

---
+++ section-1/fahrenheit .js
@@ -0,0 +1,6 @@
+// ((f-32)*5)/9
+
+var f = 35;
+
+let cel = ((f-32)*5)/9;
+console.log(cel);
---
+++ section-1/prime.js
@@ -0,0 +1,20 @@
+num = 2;
+k = 0;
+for(let i = 1; i <= 100; i++)
+{
+    if(num%i === 0){
+        k = k+1
+    }
+}
+        if(k === 2)
+        {
+            console.log("number is prime\n");
+        }
+        else{
+            console.log("number is not prime\n");
+
+        }
+    
+
+
+console.log("everything is fine");
