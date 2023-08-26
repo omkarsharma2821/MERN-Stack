0c427a9ad7716da9ee562da0f07d573204f51e1b
Omkar Sharma <omkarsharma2821@gmail.com>
2023-08-27 00:14:17 +0530

    js

---
+++ section-1/assignment-26.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4,5];
+for(let i = 0; i<5; i++){
+//  console.log(arr[i]*arr[i]);
+const arr2 = arr[i]*arr[i];
+console.log(arr2);
+}
+
+console.log('<------------------------>')
+
+const a = [1,2,3,4,5,6,7,8,9,10];
+for(let i = 0; i <10; i++){
+    if(a[i]%2 === 0){
+        const even_array = a[i];
+        console.log(even_array);
+    }
+}
