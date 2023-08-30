7b7cae98087fd332c54464bdce537b64cb28b047
Omkar Sharma <omkarsharma2821@gmail.com>
2023-08-30 10:59:03 +0530

    Route

--- section 2/frontend/src/App.jsx
+++ section 2/frontend/src/App.jsx
@@ -3,17 +3,23 @@ import './App.css';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div>
       <h1>My react tab</h1>
 
-      <Home />
-      
-        <Signup />
-
-        <Login />
+        <BrowserRouter>
+        {/* rouetes mtlb address create krna */}
+         <Routes> 
+            <Route path= "/home" element= {<Home />}/>
+            <Route path= "/login" element= {<Login />}/>
+            <Route path= "/signup" element= {<Signup />}/>
+            <Route path= "*" element= {<NotFound />}/>
+         </Routes>
+        </BrowserRouter>
      
     </div>
   );
