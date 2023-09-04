fbc4b54c23fcba1f297c38d01ed53455088a333a
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-04 08:58:22 +0530

    react

--- section 2/frontend/src/components/Chat.jsx
+++ section 2/frontend/src/components/Chat.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
 const Chat = () => {
-    const [todolist, setTodolist] = useState([]);
+    const [chatlist, setchatlist] = useState([]);
     
-    const addNewTodo = (e) =>{
+    const addNewChat = (e) =>{
         // console.log(e.code);
         if(e.code === 'Enter'){
             console.log(e.target.value)
-            setTodolist([...todolist, e.target.value]);
-            console.log([...todolist, e.target.value]);
+            setchatlist([...chatlist, e.target.value]);
+            console.log([...chatlist, e.target.value]);
         }
     }
     
@@ -18,14 +18,14 @@ const Chat = () => {
             <h1 className="text-center my-5">ChatApp</h1>
             <div className="card p-5 w-100">
                 <div className="card-header ">
-                    <input type="text" className="form-control" onKeyDown={addNewTodo}/>
+                    <input type="text" className="form-control" onKeyDown={addNewChat}/>
                 </div>
                     {/* d-flex se ek row me */}
                 <div className="card-body">
-                    {todolist.map( (todo) => {
+                    {chatlist.map( (chat) => {
                         return (
                         <div className='d-flex justify-content-between mb-3'>
-                        <h5>{todo}</h5>
+                        <h5>{chat}</h5>
                         <button className="btn btn-danger">Send</button>
                         </div>
                     );
