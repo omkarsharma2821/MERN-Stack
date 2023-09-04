6f89db6615f9b036c5a8b5dec3fc213cfc1b4df6
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-04 11:55:23 +0530

    todp

--- section 2/frontend/src/components/Todo.jsx
+++ section 2/frontend/src/components/Todo.jsx
@@ -5,12 +5,21 @@ const Todo = () => {
     
     const addNewTodo = (e) =>{
         // console.log(e.code);
+        if(!e.target.value.trim()) return;
         if(e.code === 'Enter'){
             console.log(e.target.value)
             // todolist.push(e.target.value);
-            setTodolist([...todolist, e.target.value]);
-            console.log([...todolist, e.target.value]);
+            setTodolist([...todolist, e.target.value]); // add new value to new value 
+            console.log([...todolist, e.target.value]); 
+            e.target.value= " ";
         }
+    };
+
+    const removeTodo = (index) =>{
+        console.log(index);
+        const temp = todolist;
+        temp.splice(index, 1);
+        setTodolist([...temp]);
     }
     
   return (
@@ -23,11 +32,11 @@ const Todo = () => {
                 </div>
                     {/* d-flex se ek row me */}
                 <div className="card-body">
-                    {todolist.map( (todo) => {
+                    {todolist.map( (todo, index) => {
                         return (
                         <div className='d-flex justify-content-between mb-3'>
                         <h5>{todo}</h5>
-                        <button className="btn btn-danger">Delete</button>
+                        <button className="btn btn-danger" onClick={() => {removeTodo(index)}} >Delete</button>
                         </div>
                     );
                     })}
@@ -38,4 +47,4 @@ const Todo = () => {
   )
 }
 
-export default Todo
+export default Todo;
