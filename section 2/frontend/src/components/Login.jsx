569914748569406725676ad49de87c0eca1632f7
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-08 14:04:55 +0530

    formik

--- section 2/frontend/src/components/Login.jsx
+++ section 2/frontend/src/components/Login.jsx
@@ -4,21 +4,9 @@ import * as Yup from 'yup';
 const SignupSchema = Yup.object().shape({
 
   firstName: Yup.string()
-
     .min(2, 'Too Short!')
-
     .max(50, 'Too Long!')
-
     .required('Required'),
-
-  lastName: Yup.string()
-
-    .min(2, 'Too Short!')
-
-    .max(50, 'Too Long!')
-
-    .required('Required'),
-
   email: Yup.string().email('Invalid email').required('Required'),
 
 });
